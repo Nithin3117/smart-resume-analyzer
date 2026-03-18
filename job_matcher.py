baa79a86de35a42cdff9efaf1047c7d8186a4985
Nithin Bollineni <nithinbollineni04@gmail.com>
2026-03-18 22:47:28 +0530

    Update job_matcher.py

--- job_matcher.py
+++ job_matcher.py
@@ -1,27 +1,33 @@
-def extract_job_skills(job_text, skills_list):
+def extract_job_skills(job_text,skills_list):
 
     job_text = job_text.lower()
 
     job_skills = []
 
     for skill in skills_list:
+
         if skill in job_text:
+
             job_skills.append(skill)
 
     return job_skills
 
 
-def calculate_match(resume_skills, job_skills):
+def calculate_match(resume_skills,job_skills):
 
     resume_set = set(resume_skills)
     job_set = set(job_skills)
 
     matched = resume_set.intersection(job_set)
+
     missing = job_set - resume_set
 
     if len(job_skills) == 0:
+
         score = 0
+
     else:
+
         score = (len(matched) / len(job_skills)) * 100
 
-    return score, matched, missing
+    return score,matched,missing
