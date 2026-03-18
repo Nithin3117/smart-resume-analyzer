bf1d8305ddb1520e17778deebfe0a73fe23e7eab
Nithin Bollineni <nithinbollineni04@gmail.com>
2026-03-18 22:35:05 +0530

    Fix job text extraction with separator

--- job_link_extractor.py
+++ job_link_extractor.py
@@ -2,10 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 def extract_job_text(url):
+
     try:
         response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        text = soup.get_text()
+
+        soup = BeautifulSoup(response.text,"html.parser")
+
+        text = soup.get_text(separator=" ")
+
         return text.lower()
+
     except:
         return ""
