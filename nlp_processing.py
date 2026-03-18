329f770fd77bfdfd78ceb4c52b4c5faca91902c4
Nithin Bollineni <nithinbollineni04@gmail.com>
2026-03-18 22:49:32 +0530

    Refactor preprocess function and clean up code

--- nlp_processing.py
+++ nlp_processing.py
@@ -1,25 +1,22 @@
 import nltk
-
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('punkt_tab')
-
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+nltk.download('punkt')
+nltk.download('stopwords')
+
 def preprocess(text):
 
-    # convert text to lowercase
     text = text.lower()
 
-    # split text into words
     tokens = word_tokenize(text)
 
-    # remove unnecessary words
-    filtered_words = []
+    filtered = []
 
     for word in tokens:
-        if word.isalpha() and word not in stopwords.words('english'):
-            filtered_words.append(word)
 
-    return filtered_words
+        if word.isalpha() and word not in stopwords.words('english'):
+
+            filtered.append(word)
+
+    return filtered
