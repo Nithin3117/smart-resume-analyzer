aec7ef65f576f26282e5449006f1a271a9156b2b
Nithin Bollineni <nithinbollineni04@gmail.com>
2026-03-19 16:23:22 +0530

    Refactor preprocess function for tokenization and stopwords
    Updated the preprocess function to use simple tokenization and a custom stopwords list.

--- nlp_processing.py
+++ nlp_processing.py
@@ -1,32 +1,27 @@
-import nltk
-import os
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
 import string
 
-# -------- SET NLTK DATA PATH --------
-nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
-if not os.path.exists(nltk_data_path):
-    os.makedirs(nltk_data_path)
-
-nltk.data.path.append(nltk_data_path)
-
-# -------- DOWNLOAD REQUIRED DATA --------
-nltk.download('punkt', download_dir=nltk_data_path)
-nltk.download('punkt_tab', download_dir=nltk_data_path)
-nltk.download('stopwords', download_dir=nltk_data_path)
-
-
-# -------- PREPROCESS FUNCTION --------
 def preprocess(text):
 
+    # Convert to lowercase
     text = text.lower()
 
-    tokens = word_tokenize(text)
+    # Simple tokenization (split by spaces)
+    tokens = text.split()
 
-    tokens = [word for word in tokens if word not in string.punctuation]
+    # Remove punctuation
+    tokens = [word.strip(string.punctuation) for word in tokens]
 
-    stop_words = set(stopwords.words('english'))
+    # Remove empty tokens
+    tokens = [word for word in tokens if word != ""]
+
+    # Simple stopwords list
+    stop_words = {
+        "the", "and", "is", "in", "to", "of", "for", "on", "with",
+        "as", "by", "an", "at", "from", "or", "that", "this",
+        "it", "be", "are", "was", "were"
+    }
+
+    # Remove stopwords
     tokens = [word for word in tokens if word not in stop_words]
 
     return tokens
