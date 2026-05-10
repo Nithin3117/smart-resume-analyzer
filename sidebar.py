af5a1b38915fe451f1dc69506ec747703bdd6841
Nithin Bollineni <nithinbollineni04@gmail.com>
2026-05-10 15:25:15 +0530

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -1,33 +1,11 @@
 import streamlit as st
 
-from chatbot import chatbot_response
-
 
 # =====================================================
 # SIDEBAR
 # =====================================================
 
 def show_sidebar():
-
-    st.sidebar.title(
-        "🤖 AI Resume Assistant"
-    )
-
-    user_question = st.sidebar.text_input(
-        "Ask something about your resume"
-    )
-
-    if user_question:
-
-        answer = chatbot_response(
-            user_question
-        )
-
-        st.sidebar.success(
-            answer
-        )
-
-    st.sidebar.markdown("---")
 
     st.sidebar.title(
         "📌 Navigation"
