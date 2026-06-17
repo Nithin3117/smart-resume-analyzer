7d4ef1909ad954b11b7b82c1fc15480cc39edcbd
Nithin Bollineni <nithinbollineni04@gmail.com>
2026-06-17 10:04:10 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -4,30 +4,19 @@ import docx
 # PDF READER
 
 def extract_text_pdf(file):
-
     text = ""
-
     reader = PyPDF2.PdfReader(file)
-
     for page in reader.pages:
-
         extracted = page.extract_text()
-
         if extracted:
             text += extracted
-
     return text
 
 # DOCX READER
 
 def extract_text_docx(file):
-
     doc = docx.Document(file)
-
     text = ""
-
     for para in doc.paragraphs:
-
         text += para.text + "\n"
-
     return text
