48dffdbf0f0360cbb78363350cbb2687d061f83e
Nithin Bollineni <nithinbollineni04@gmail.com>
2026-06-17 09:59:01 +0530

    Update job_link_extractor.py

--- job_link_extractor.py
+++ job_link_extractor.py
@@ -1,22 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 def extract_job_text(url):
-
     try:
-
         headers = {
             "User-Agent": "Mozilla/5.0"
         }
-
         response = requests.get(url, headers=headers)
-
         soup = BeautifulSoup(response.text, "html.parser")
-
         text = soup.get_text(separator=" ")
-
         return text.lower()
-
     except:
         return ""
