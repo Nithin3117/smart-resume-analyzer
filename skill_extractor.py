6bebf4ab77fe8976ff651be02fb558451ea31a3d
Nithin Bollineni <nithinbollineni04@gmail.com>
2026-06-17 10:05:59 +0530

    Update skill_extractor.py

--- skill_extractor.py
+++ skill_extractor.py
@@ -1,18 +1,11 @@
 def load_skills(file_path):
-
     with open(file_path, "r") as f:
         skills = [line.strip().lower() for line in f.readlines()]
-
     return skills
 
-
 def extract_skills(tokens, skills_list):
-
     found_skills = []
-
     for token in tokens:
-
         if token.lower() in skills_list:
             found_skills.append(token.lower())
-
     return list(set(found_skills))
