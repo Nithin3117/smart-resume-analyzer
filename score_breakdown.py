14518e76875954b5e62d5411796b48752242e2b5
Nithin Bollineni <nithinbollineni04@gmail.com>
2026-06-28 17:22:40 +0530

    Update score_breakdown.py

--- score_breakdown.py
+++ score_breakdown.py
@@ -7,22 +7,21 @@ def calculate_breakdown(
     projects
 ):
 
-    # ---------------- ATS SCORE ----------------
-
+    # ATS SCORE
     ats_score = int(score)
 
-    # ---------------- SKILLS SCORE ----------------
-
+    # SKILLS SCORE
     total_skills = len(matched) + len(missing)
     if total_skills == 0:
         skills_score = 0
     else:
-        skills_score = int((len(matched) / total_skills) * 100)
+        skills_score = int(
+            (len(matched) / total_skills) * 100
+        )
 
-    # ---------------- EDUCATION SCORE ----------------
-
-    education_score = 
-    if education
+    # EDUCATION SCORE
+    education_score = 0
+    if education:
         for item in education:
             text = item.lower()
             if "b.tech" in text or "btech" in text:
@@ -31,19 +30,25 @@ def calculate_breakdown(
                 education_score += 25
             elif "ssc" in text or "10th" in text:
                 education_score += 25
-        education_score = min(education_score,100)
+            elif "cgpa" in text or "%" in text:
+                education_score += 10
+        education_score = min(education_score, 100)
 
-    # ---------------- EXPERIENCE SCORE ----------------
-
+    # EXPERIENCE SCORE
     experience_score = 0
     if experience:
-        experience_score = min(len(experience) * 30, 100)
+        experience_score = min(
+            len(experience) * 30,
+            100
+        )
 
-    # ---------------- PROJECT SCORE ----------------
-
+    # PROJECT SCORE
     project_score = 0
     if projects:
-        project_score = min(len(projects) * 30, 100)
+        project_score = min(
+            len(projects) * 30,
+            100
+        )
 
     return {
         "ATS Compatibility": ats_score,
@@ -51,4 +56,5 @@ def calculate_breakdown(
         "Education Strength": education_score,
         "Experience Strength": experience_score,
         "Project Strength": project_score
+
     }
