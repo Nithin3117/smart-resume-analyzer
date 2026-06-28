2eae6a15c178a62f98c691384908b9f5e659a678
Nithin Bollineni <nithinbollineni04@gmail.com>
2026-06-28 17:31:35 +0530

    Calculate and return detailed score metrics

--- score_breakdown.py
+++ score_breakdown.py
@@ -6,10 +6,55 @@ def calculate_breakdown(
     experience,
     projects
 ):
+
+    # ATS SCORE
+    ats_score = int(score)
+
+    # SKILLS SCORE
+    total_skills = len(matched) + len(missing)
+    if total_skills == 0:
+        skills_score = 0
+    else:
+        skills_score = int(
+            (len(matched) / total_skills) * 100
+        )
+
+    # EDUCATION SCORE
+    education_score = 0
+    if education:
+        for item in education:
+            text = item.lower()
+            if "b.tech" in text or "btech" in text:
+                education_score += 50
+            elif "intermediate" in text or "+2" in text:
+                education_score += 25
+            elif "ssc" in text or "10th" in text:
+                education_score += 25
+            elif "cgpa" in text or "%" in text:
+                education_score += 10
+        education_score = min(education_score, 100)
+
+    # EXPERIENCE SCORE
+    experience_score = 0
+    if experience:
+        experience_score = min(
+            len(experience) * 30,
+            100
+        )
+
+    # PROJECT SCORE
+    project_score = 0
+    if projects:
+        project_score = min(
+            len(projects) * 30,
+            100
+        )
+
     return {
-        "ATS Compatibility": int(score),
-        "Skills Match": 0,
-        "Education Strength": 0,
-        "Experience Strength": 0,
-        "Project Strength": 0,
+        "ATS Compatibility": ats_score,
+        "Skills Match": skills_score,
+        "Education Strength": education_score,
+        "Experience Strength": experience_score,
+        "Project Strength": project_score
+
     }
